0f36bdc770b47079a9e13ee05226bdb3d89cf4f3
6blCTPOmiha <bystrovmiha@gmail.com>
2026-04-09 01:17:42 +0400

    Класс проверок на странице membership

---
+++ pages/membership/membership_checks.py
@@ -0,0 +1,15 @@
+from helpers.base_page import BasePage
+from data.locators import Locators
+
+
+class MembershipChecks(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+        self.locators = Locators()
+
+
+    def check_title(self):
+        title = self.find_element(self.locators.LMC_TITLE)
+        assert title.text == "LIFETIME MEMBERSHIP CLUB"
+
