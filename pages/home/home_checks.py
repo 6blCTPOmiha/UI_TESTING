3d2254cdaeb8d7948518658cafb732657830ef36
6blCTPOmiha <bystrovmiha@gmail.com>
2026-04-12 20:57:28 +0400

    self.locators заменены на Locators

--- pages/home/home_checks.py
+++ pages/home/home_checks.py
@@ -1,77 +1,85 @@
 from helpers.base_page import BasePage
 from data.locators import Locators
+from pages.home.home_page import HomePage
 
 
 class HomeChecks(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-        self.locators = Locators()
 
+    def check_header(self):
+        self.wait_for_element_visible_for_user(Locators.HEADER_PART)
+        assert self.wait_for_element_visible_for_user(Locators.HEADER_PART), 'хедер с контактной информацией отсутствует'
 
     def check_navigation_panel(self):
-        assert self.is_element_on_screen(self.locators.NAVIGATION_PANEL), 'Отсутствует панель навигации'
+        self.wait_for_element_visible_for_user(Locators.NAVIGATION_PANEL)
+        assert self.is_element_on_screen(Locators.NAVIGATION_PANEL), 'Отсутствует панель навигации'
 
 
     def check_reg_btn(self):
-        el = self.find_elements(self.locators.REGISTER_BTN)
+        el = self.find_elements(Locators.REGISTER_BTN)
         assert len(el) > 0, 'Кнопка регистрации отсутствует'
 
     def check_list_of_courses(self):
-        assert self.is_element_on_screen(self.locators.BEST_SELENIUM_COURSE), 'Список курсов отсутствует'
+        assert self.is_element_on_screen(Locators.BEST_SELENIUM_COURSE), 'Список курсов отсутствует'
 
 
     def check_tel_number_ind(self):
-        assert self.is_element_on_screen(self.locators.HEADER_IND_NUMBER), 'Телефон отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_IND_NUMBER), 'Телефон отсутствует'
 
 
     def check_tel_number_wa(self):
-        assert self.is_element_on_screen(self.locators.HEADER_WHATSUP_NUMBER), 'Телефон отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_WHATSUP_NUMBER), 'Телефон отсутствует'
 
 
     def check_tel_number_usa(self):
-        assert self.is_element_on_screen(self.locators.HEADER_USA_NUMBER), 'Телефон отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_USA_NUMBER), 'Телефон отсутствует'
 
 
     def check_skype(self):
-        assert self.is_element_on_screen(self.locators.HEADER_SKYPE_ID), 'Скайп отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_SKYPE_ID), 'Скайп отсутствует'
 
 
     def check_top_email(self):
-        assert self.is_element_on_screen(self.locators.HEADER_EMAIL), 'Электронная почта отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_EMAIL), 'Электронная почта отсутствует'
 
 
     def check_facebook(self):
-        assert self.is_element_on_screen(self.locators.HEADER_FACEBOOK), 'facebook отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_FACEBOOK), 'facebook отсутствует'
 
 
     def check_linkedin(self):
-        assert self.is_element_on_screen(self.locators.HEADER_LINKEDIN), 'linkedin отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_LINKEDIN), 'linkedin отсутствует'
 
 
     def check_google(self):
-        assert self.is_element_on_screen(self.locators.HEADER_GOOGLE), 'google_plus отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_GOOGLE), 'google_plus отсутствует'
 
 
     def check_youtube(self):
-        assert self.is_element_on_screen(self.locators.HEADER_YOUTUBE), 'youtube отсутствует'
+        assert self.is_element_on_screen(Locators.HEADER_YOUTUBE), 'youtube отсутствует'
 
+    def check_footer(self):
+        self.wait_for_element_visible_for_user(Locators.FOOTER_PART)
+        assert self.is_element_on_screen(Locators.FOOTER_PART)
 
     def check_footer_address(self):
-        assert self.is_element_on_screen(self.locators.FOOTER_ADDRESS), 'Адрес отсутствует'
+        self.wait_for_element_visible_for_user(Locators.FOOTER_PART)
+        assert self.is_element_on_screen(Locators.FOOTER_ADDRESS), 'Адрес отсутствует'
 
 
     def check_footer_tel_1(self):
-        assert self.is_element_on_screen(self.locators.FOOTER_TEL_1), 'Первый контактный телефон отсутствует'
+        assert self.is_element_on_screen(Locators.FOOTER_TEL_1), 'Первый контактный телефон отсутствует'
 
 
     def check_footer_tel_2(self):
-        assert self.is_element_on_screen(self.locators.FOOTER_TEL_2), 'Второй контактный телефон отсутствует'
+        assert self.is_element_on_screen(Locators.FOOTER_TEL_2), 'Второй контактный телефон отсутствует'
 
 
     def check_footer_email_1(self):
-        assert self.is_element_on_screen(self.locators.FOOTER_EMAIL_1), 'Первая контактная электронная почта отсутствует'
+        assert self.is_element_on_screen(Locators.FOOTER_EMAIL_1), 'Первая контактная электронная почта отсутствует'
 
 
     def check_footer_email_2(self):
-        assert self.is_element_on_screen(self.locators.FOOTER_EMAIL_2), 'Вторая контактная электронная почта отсутствует'
+        assert self.is_element_on_screen(Locators.FOOTER_EMAIL_2), 'Вторая контактная электронная почта отсутствует'
--- pages/membership/membership_checks.py
+++ pages/membership/membership_checks.py
@@ -6,10 +6,9 @@ class MembershipChecks(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-        self.locators = Locators()
-
 
     def check_title(self):
-        title = self.find_element(self.locators.LMC_TITLE)
+        self.find_element(Locators.MEMBER_BG_IMG_ON_TOP)
+        title = self.find_element(Locators.LMC_TITLE)
         assert title.text == "LIFETIME MEMBERSHIP CLUB", 'Отсутствует заголовок'
 
