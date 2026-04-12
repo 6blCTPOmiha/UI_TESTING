e4443d750e671ca70992dd4d44126bca3a4b66eb
6blCTPOmiha <bystrovmiha@gmail.com>
2026-04-12 20:56:25 +0400

    тесты переработаны в соответствии с требованиями

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,21 +2,16 @@ class TestRun:
 
     def test_page_loading(self, home_page, home_checks):
         home_page.open_page()
-        home_page.wait_for_page_load()
-        home_page.wait_for_header_load()
-        home_checks.check_skype()
-        home_page.wait_for_navig_load()
+        home_checks.check_header()
         home_checks.check_navigation_panel()
         home_checks.check_reg_btn()
         home_checks.check_list_of_courses()
-        home_page.tp_down()
-        home_page.wait_for_footer_load()
-        home_checks.check_footer_tel_1()
+        home_page.scroll_to_the_footer()
+        home_checks.check_footer()
 
 
     def test_header(self, home_page, home_checks):
         home_page.open_page()
-        home_page.wait_for_page_load()
         home_checks.check_tel_number_ind()
         home_checks.check_tel_number_wa()
         home_checks.check_tel_number_usa()
@@ -30,10 +25,7 @@ class TestRun:
 
     def test_footer(self, home_page, home_checks):
         home_page.open_page()
-        home_page.wait_for_page_load()
-        home_page.wait_for_navig_load()
-        home_page.tp_down()
-        home_page.wait_for_footer_load()
+        home_page.scroll_to_the_footer()
         home_checks.check_footer_address()
         home_checks.check_footer_tel_1()
         home_checks.check_footer_tel_2()
@@ -43,23 +35,15 @@ class TestRun:
 
     def test_navigation_menu_on_scroll(self, home_page, home_checks):
         home_page.open_page()
-        home_page.wait_for_page_load()
-        home_page.wait_for_navig_load()
         home_checks.check_navigation_panel()
         home_page.scroll_a_little_down()
-        home_page.wait_for_navig_load()
         home_checks.check_navigation_panel()
         home_page.scroll_a_little_down()
-        home_page.wait_for_navig_load()
         home_checks.check_navigation_panel()
 
 
     def test_membership_page(self, home_page, membership_page, membership_checks):
         home_page.open_page()
-        home_page.wait_for_page_load()
-        home_page.wait_for_all_courses_dropdown_trigger_load()
         home_page.click_on_all_courses()
-        home_page.wait_for_lifetime_membership_btn_load()
         home_page.click_on_lifetime_membership()
-        membership_page.wait_for_page_load()
         membership_checks.check_title()
