395ddc101180dae18ed47f639426213101cd6e11
6blCTPOmiha <bystrovmiha@gmail.com>
2026-04-15 19:07:16 +0400

    Описание юзернейма вынесено из параметризации

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,16 +9,16 @@ class TestRun:
     @allure.story("Login test")
     @allure.severity(allure.severity_level.CRITICAL)
     @pytest.mark.login
-    @pytest.mark.parametrize('username, password, username_description',
-                             [('angular', 'password', 'username_description'),
-                              ('a', 'password', 'username_description'),
-                              ('angular', 'p', 'username_description'),
-                              ('a', 'p', 'username_description')])
-    def test_page_loading(self, login_page, login_checks, username, password, username_description):
+    @pytest.mark.parametrize('username, password',
+                             [('angular', 'password'),
+                              ('a', 'password'),
+                              ('angular', 'p'),
+                              ('a', 'p')])
+    def test_page_loading(self, login_page, login_checks, username, password):
         login_page.open_page()
         login_page.input_username(username)
         login_page.input_password(password)
-        login_page.input_username_description(username_description)
+        login_page.input_username_description('username_description')
         login_page.click_login_btn()
         login_checks.check_login_success()
 
