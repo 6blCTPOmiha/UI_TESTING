f08a80d7acb9e05f638e8508f8b1f823d66690bc
6blCTPOmiha <bystrovmiha@gmail.com>
2026-04-15 19:03:45 +0400

    Файл конфигурации содержащий базовый юрл, таймауты и пути к различным страницам, которые используются в проекте

---
+++ config.py
@@ -0,0 +1,11 @@
+class Config:
+    BASE_URL = "https://www.way2automation.com"
+    SHORT_TIMEOUT = 5
+    TIMEOUT = 15
+    LONG_TIMEOUT = 60
+
+
+class Endpoints:
+    HOME = "/"
+    LOGIN = "/angularjs-protractor/registeration/#/login"
+    MEMBERSHIP = "/lifetime-membership-club/"
