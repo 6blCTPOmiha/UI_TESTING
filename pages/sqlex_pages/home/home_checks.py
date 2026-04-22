872292b936ec266ea6b701a95ddc2c651ea75e42
6blCTPOmiha <bystrovmiha@gmail.com>
2026-04-22 15:07:12 +0400

    новые методы для работы с jse функциями

--- pages/sqlex_pages/home/home_checks.py
+++ pages/sqlex_pages/home/home_checks.py
@@ -8,8 +8,17 @@ class HomeChecks(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-
     @allure.step('Проверка. Вход в аккаунт успешный')
     def check_login_success(self):
         name = self.find_element(Locators.TEST_NAME_A)
         assert name.text == TextData.TEST_NAME, 'Не удалось авторизоваться'
+
+    @allure.step('Проверка. Фокус НЕ на input')
+    def check_no_focus_on_input(self):
+        active = self.return_focus_part()
+        assert active != "INPUT", "Фокус остался на input"
+
+    @allure.step('Проверка. Страница имеет скролл')
+    def check_page_has_scroll(self):
+        flag = self.is_page_has_scroll()
+        assert flag, "Страница не имеет скролла"
