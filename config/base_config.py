6622f79919d11ced57d64d34cd7116f51640ba8d
6blCTPOmiha <bystrovmiha@gmail.com>
2026-04-28 21:43:49 +0400

    Обновлён конфиг для корректной работы различных способов запуска прогона тестов

--- config/base_config.py
+++ config/base_config.py
@@ -1,4 +1,10 @@
+import os
+
+
 class Config:
     SHORT_TIMEOUT = 5
     TIMEOUT = 15
     LONG_TIMEOUT = 60
+    BROWSER = os.getenv("BROWSER", "chrome")
+    REMOTE = os.getenv("REMOTE", "false").lower() == "true"
+    GRID_URL = os.getenv("GRID_URL", "http://localhost:4444/wd/hub")
